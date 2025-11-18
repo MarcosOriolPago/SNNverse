793a18f77b2c2be2bf457895bfca3c7bb9da62e6
MarcosOriolPago <marcos.oriol.p@gmail.com>
2025-11-18 13:29:40 +0100

    feat: stablished communication via FastApi & fixed voltage color & added dragable LIF neuron block

--- front/src/components/layout/MainContent.tsx
+++ front/src/components/layout/MainContent.tsx
@@ -1,6 +1,6 @@
-// MainContent.tsx
 import React from 'react';
 import NodeFlowLayout from '../NodeLayout';
+import { FiPlay } from 'react-icons/fi';
 
 interface MainContentProps {
     isCollapsed: boolean;
@@ -10,7 +10,7 @@ const MainContent: React.FC<MainContentProps> = ({ isCollapsed }) => {
     const contentMargin = isCollapsed ? 'ml-16' : 'ml-64';
 
     return (
-        <main className={`flex-1 p-8 transition-margin duration-300 ease-in-out ${contentMargin}`}>
+        <main className={`flex-1 relative h-screen bg-gray-50 dark:bg-gray-900 transition-all duration-300 ease-in-out ${contentMargin}`}>
             <NodeFlowLayout />
         </main>
     );
