18b44ea68ee90717d72fad7397950c508c5f3550
MarcosOriolPago <marcos.oriol.p@gmail.com>
2026-02-26 16:17:22 +0100

    fix: ensured python version and libraries were same in build and in running

--- front/src/pages/Studio.tsx
+++ front/src/pages/Studio.tsx
@@ -256,7 +256,7 @@ const StudioContent = () => {
                                 <span className="font-medium text-sm mr-2">Home</span>
                             </button>
                         </div>
-                        <div className="absolute top-4 right-4 z-50">
+                        <div className="absolute bottom-4 left-4 z-50">
                             <UserMenu />
                         </div>
 
